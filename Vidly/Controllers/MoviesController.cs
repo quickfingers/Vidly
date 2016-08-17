6aac87017c85ca5fc1f77b4cc860dae5d84251b2
Efthemios Suyat <ESuyat@cdmny.com>
2016-08-17 12:15:12 -0400

    update styles to paper

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -15,5 +15,11 @@ namespace Vidly.Controllers
             var movie = new Movie() {Name = "Shrek"};
             return View(movie);
         }
+
+        public ActionResult Test()
+        {
+            return View("Test");
+
+        }
     }
 }
