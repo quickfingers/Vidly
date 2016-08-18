f82ba6a0f2c80ee66b3d6170030a911083c63695
quickfingersdev <quickfingersdev@gmail.com>
2016-08-17 23:36:00 -0400

    working with entity framework

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +13,15 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
+            /*
+            routes.MapRoute(
+                "MovieByReleaseData",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate"},
+                new { year= @"\d{4}", month = @"\d{2}"});
+                */
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
